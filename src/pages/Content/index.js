cebdc41a7b2e33ba135139e7bf42d928e5ad1478
PublicCodeRobin <86063078+PublicCodeRobin@users.noreply.github.com>
2021-12-01 13:24:44 +0100

    init

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,3 +4,5 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+const checks = document.querySelectorAll('input[type="checkbox"]');
+console.log(checks);
